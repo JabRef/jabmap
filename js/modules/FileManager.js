04367beb3aadec5b5d89390adf6b7c619b565ced
Robert Koch <robert.koch@student.htw-berlin.de>
2025-05-17 22:13:06 +0200

    Created a FileManager class to include file access

---
+++ js/modules/FileManager.js
@@ -0,0 +1,22 @@
+/**
+ * Provides file access' functionality.
+ */
+class FileManager {
+    constructor() { }
+
+    /**
+     * Reads a .jmp file with the saved mind map.
+     * @param { string } path - The path of a map.
+     */
+    LoadMap(path) {
+
+    }
+
+    /**
+     * Creates or rewrites a file with corresponding mind map.
+     * @param { map } mindMap - The mind map to save into a file.
+     */
+    SaveMap(mindMap) {
+
+    }
+}
